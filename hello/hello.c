0cabe8f402bc414b86a69ee9e04a5c030cf1d069
supershang <shangshipei@163.com>
2017-04-07 11:14:16 +0800

    first commit

---
+++ hello/hello.c
@@ -0,0 +1,25 @@
+/*-------------------------------------------------------------------------
+    > File Name :	 hello.c
+    > Author :		shang
+    > Mail :		shangshipei@gmail.com 
+    > Description :	shang 
+    > Created Time :	2017年04月07日 星期五 10时28分51秒
+    > Rev :		0.1
+ ------------------------------------------------------------------------*/
+#include <linux/init.h>
+#include <linux/module.h>
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int hello_init(void)
+{
+	printk(KERN_EMERG "Hello. world\n");
+	return 0;
+}
+
+static void hello_exit(void)
+{
+	printk(KERN_EMERG "Goodbye, cruel world\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
